6796c290bde9b086eb177df76eef159f9fba78d3
Jose Villalobos <jose.villalobos@wizard-3d.com>
2019-09-23 13:42:35 -0500

    Writing computing densities and compute acceleration equations

---
+++ src/sph/index.ts
@@ -0,0 +1,106 @@
+import { SystemParameters, SystemState } from '../types';
+
+export function getParameters(parameters:SystemParameters) {
+    throw 'Not implemented';
+}
+
+export function allocState(n:number) {
+    throw 'Not implemented';
+}
+
+export function freeState(state:SystemState) {
+    throw 'Not implemented';
+}
+
+export function computeDensity(state:SystemState, parameters:SystemParameters) {
+    const { n, rho, x, mass } = state;
+    const h = parameters.h;
+    const h2 = h*h;
+    const h8 = h2*h2*h2*h2;
+    const C = 4 * mass / Math.PI / h8;
+    const C2 = 4 * mass / Math.PI / h2;
+
+    // We search for neighbors of node i by checking every particle, which is not
+    // very efficient. We do at least take advange of the symmetry of the update (i
+    // contributes to j in the same way that j contributes to i).
+
+    // for each particle
+    for (let i = 0; i < n; i++) {
+        rho[i] += C2;
+        // iterate each other particle
+        for (let j = i + 1; i < n; j++) {
+            const dx = x[2*i + 0] - x[2*j + 0];
+            const dy = x[2*i + 1] - x[2*j + 1];
+            const r2 = dx + dy;
+            const z = h2 - r2;
+            if (z > 0) {
+                const rho_ij = C*z*z*z;
+                rho[i] += rho_ij;
+                rho[j] += rho_ij;
+            }
+        }
+    }
+}
+
+/**
+ * Like compute density,
+ * the compute accel routine takes advantage of the symmetry of the interaction
+ * forces (finteract_ij = −finteract_ji )
+ * but it does a very expensive brute force search for
+ * neighbors.
+ * TODO: Apply the Barnes-Hut algorithm which cuts the complexity of n-body simulations from  ∼O(n2)  to  ∼O(nlog(n)).
+ * @param state 
+ * @param parameters 
+ */
+export function computeAcceleration(state:SystemState, parameters:SystemParameters) {
+    // unpack basic parameters
+    const {h, rho0, k, mu, g} = parameters;
+    const h2 = h*h;
+    // unpack system state
+    const { mass, rho, x, v, a, n } = state;
+
+    // compute density
+    computeDensity(state, parameters);
+
+    // add gravity to each particle
+    for (let i = 0; i < n; i++) {
+        a[2*i + 0] = 0; // x component
+        a[2*i + 1] = -g; // y component
+    }
+
+    // constants for interaction terms
+    const Co = mass / Math.PI / (h2*h2);
+    const Cp = 15*k;
+    const Cv = -40*mu;
+
+    // Compute interaction forces
+    // for each particle
+    for (let i = 0; i < n; i++) {
+        const rhoi = rho[i];
+        // iterate each other particle
+        for (let j = i + 1; i < n; j++) {
+            const dx = x[2*i + 0] - x[2*j + 0];
+            const dy = x[2*i + 1] - x[2*j + 1];
+            const r2 = dx + dy;
+            // is the j particle is in the radious h of i?
+            if (r2 < h2) {
+                const rhoj = rho[j];
+                const q = Math.sqrt(r2)/h;
+                const u = 1 - q;
+                // TODO: Remove rhoi from wo since it don't appears on the paper's equations
+                const wo = Co * u / rhoi / rhoj;
+                const wp = wo * Cp * (rhoi + rhoj - 2*rho0) * u/q;
+                const wv = wo*Cv;
+
+                const dvx = v[2*i + 0] - v[2*j + 0];
+                const dvy = v[2*i + 1] - v[2*j + 1];
+
+                // TODO: Understand this.
+                a[2*i+0] += (wp*dx + wv*dvx);
+                a[2*i+1] += (wp*dy + wv*dvy);
+                a[2*j+0] -= (wp*dx + wv*dvx);
+                a[2*j+1] -= (wp*dy + wv*dvy);
+            }
+        }
+    }
+}
--- src/types.ts
+++ src/types.ts
@@ -7,3 +7,34 @@ export interface Dimension {
     width:number,
     height:number
 };
+
+/**
+ * Holds the parameters that describe the simulation
+ * These parameters are filled in by the getParams function
+ */
+export interface SystemParameters {
+    npframe:number, // steps per frame
+    h:number, /* Particle size */
+    dt:number, /* Time step */
+    rho0:number, /* Reference density */
+    k:number, /* Bulk modulus */
+    mu:number, /* Viscosity */
+    g:number /* Gravity strength */
+}
+
+/**
+ * holds the information for the current state of the
+ * system and of the integration algorithm.
+ * 
+ * The alloc state and free state functions take care of storage for the local
+ * simulation state.
+ */
+export interface SystemState {
+    n:number, /* Number of particles */
+    mass:number, /* Particle mass */
+    rho:Float32Array, /* Densities */
+    x:Float32Array, /* Positions */
+    vh:Float32Array, /* Velocities (half step) */
+    v:Float32Array, /* Velocities (full step) */
+    a:Float32Array /* Acceleration */
+}
